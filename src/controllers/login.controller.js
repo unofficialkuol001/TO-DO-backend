d678b65b36e0c3356c131b7c2a9bf11e1f2bdf82
Kuol Magok <heiskuol001@gmail.com>
2026-01-18 08:18:52 +0300

    fix: update JWT expiration time from 1 hour to 1 minute

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -29,7 +29,7 @@ const login = async (req, res) => {
         const token = jwt.sign(
             { id: user._id, name: user.name },
             process.env.JWT_SECRET_KEY,
-            {'expiresIn':'1h'}
+            {'expiresIn':'1m'}
         )
 
         //success message
